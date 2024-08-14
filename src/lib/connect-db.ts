ba1428c17a8047cf41056c134792e93a60740ce8
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-08-14 17:28:04 +0900

    Feat: DB Connect Function

---
+++ src/lib/connect-db.ts
@@ -0,0 +1,26 @@
+import { connect, Mongoose } from "mongoose";
+
+const { DB_URL, DB_NAME } = process.env
+
+if (!DB_URL) {
+  throw new Error('DB_URL must be defined');
+}
+
+let _dbConnCache: Mongoose | null = null;
+
+export const connectDb = async () => {
+  try {
+    if (_dbConnCache) {
+      return _dbConnCache;
+    }
+
+    _dbConnCache = await connect(DB_URL, {
+      dbName: DB_NAME,
+    })
+
+    return _dbConnCache;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
